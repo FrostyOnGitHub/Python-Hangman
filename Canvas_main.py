f0e0f7b6274399c646ea4ee05a1a27eb9709c2aa
FrostyOnGitHub <74784077+FrostyOnGitHub@users.noreply.github.com>
2021-04-29 16:32:28 -0400

    Update Canvas_main.py

--- Canvas_main.py
+++ Canvas_main.py
@@ -1,4 +1,4 @@
-from Tkinter import *
+from tkinter import *
 tk=Tk()
 tk.geometry('1794x700')
 tk.title("Jeu du Pendu")
@@ -13,7 +13,7 @@ message2 = Label( tk, text="Pour commencer appuyez sur JOUER", font='Times 20 bo
 message2.place(anchor='n', relx=0.5, rely=0.25, relwidth=0.8, relheight=0.355)
 
 button1 = Button(tk, text="JOUER", font='Times 10 bold', bg='spring green', fg='white', height=1, width=1)
-button1.place(anchor='n', relx=0.3, rely=0.6, relwidth=0.8, relheight=0.355)
+button1.place(anchor='n', relx=0.2, rely=0.6, relwidth=0.4, relheight=0.2)
 
 button2 = Button(tk, text="RÈGLES DU JEU", font='Times 10 bold', bg='firebrick1', fg='white', height=1, width=1)
-button2.place(anchor='n', relx=0.6, rely=0.6, relwidth=0.8, relheight=0.355)
+button2.place(anchor='n', relx=0.8, rely=0.6, relwidth=0.4, relheight=0.2)
