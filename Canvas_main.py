7a223cfeb72ceb076ab0cbf8e90930b0f70cb42c
FrostyOnGitHub <74784077+FrostyOnGitHub@users.noreply.github.com>
2021-04-29 21:24:33 -0400

    Update Canvas_main.py

--- Canvas_main.py
+++ Canvas_main.py
@@ -24,43 +24,88 @@ def regles():
     
 def jouer():
     
-    Canvas=Canvas(tk, bg="white", width=1794, height=300)
-    Canvas.place(x=0,y=400)
-    line = Canvas.create_line(0,150,1794,150)
-    line = Canvas.create_line(138,300,138,0)
-    line = Canvas.create_line(2*138,300,2*138,0)
-    line = Canvas.create_line(3*138,300,3*138,0)
-    line = Canvas.create_line(4*138,300,4*138,0)
-    line = Canvas.create_line(5*138,300,5*138,0)
-    line = Canvas.create_line(6*138,300,6*138,0)
-    line = Canvas.create_line(7*138,300,7*138,0)
-    line = Canvas.create_line(8*138,300,8*138,0)
-    line = Canvas.create_line(9*138,300,9*138,0)
-    line = Canvas.create_line(10*138,300,10*138,0)
-    line = Canvas.create_line(11*138,300,11*138,0)
-    line = Canvas.create_line(12*138,300,12*138,0)
-    line = Canvas.create_line(13*138,300,13*138,0)
-    #on crée le tableau de tableau auquel on assigne un 0 partout (pour l'ordinateur 0= case vide)
-    board=[]
-    for i in range(2):
-        board.append([])
-        for j in range(13):
-          board[i].append(0)
-          
-    def afficher():
-        for i in range(2):
-            for j in range(13):
-                if(board[j][i] == 1):
-                    croix(i,j)
-                elif(board[j][i] == 2):
-                    cercle(i,j)
+    Canvas=Canvas(tk, bg="deep sky blue", width=1780, height=470)
+    Canvas.place(x=0,y=0)
 
-    def croix(i,j):
-        line2 = Canvas.create_line(i*138,j*138,(i+1)*138,(j+1)*138,width=2,fill="red",tag="croix")
-        line3 = Canvas.create_line(i*138,j*138,(i)*138,(j+1)*138,(i+1)*138,j*138,width=2,fill="red",tag="croix")
-        
-        def cercle(i,j):
-            circle = Canvas.create_oval(i*138,j*138,(i+1)*138,(j+1)*138,width=2,tag="cercle")
+
+    button1 = Button(tk, text="A", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.050, rely=0.68, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="B", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.150, rely=0.68, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="C", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.247, rely=0.68, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="D", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.345, rely=0.68, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="E", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.443, rely=0.68, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="F", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.540, rely=0.68, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="G", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.638, rely=0.68, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="H", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.736, rely=0.68, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="I", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.833, rely=0.68, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="J", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.930, rely=0.68, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="K", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.050, rely=0.78, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="L", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.150, rely=0.78, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="M", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.247, rely=0.78, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="N", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.345, rely=0.78, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="O", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.443, rely=0.78, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="P", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.540, rely=0.78, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="Q", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.638, rely=0.78, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="R", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.736, rely=0.78, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="S", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.833, rely=0.78, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="T", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.930, rely=0.78, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="U", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.247, rely=0.88, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="V", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.345, rely=0.88, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="W", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.443, rely=0.88, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="X", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.540, rely=0.88, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="Y", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.638, rely=0.88, relwidth=0.1, relheight=0.1)
+
+    button1 = Button(tk, text="Z", font='Times 20 bold', bg='black', fg='white', height=1, width=1)
+    button1.place(anchor='n', relx=0.736, rely=0.88, relwidth=0.1, relheight=0.1)
+
             
 button1 = Button(tk, text="JOUER", font='Times 20 bold', bg='spring green', fg='black', height=1, width=1, command = jouer)
 button1.place(anchor='n', relx=0.3, rely=0.6, relwidth=0.4, relheight=0.2)
